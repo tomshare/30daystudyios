31f782030e86505bd65f39bd267208b230114fb3
唐川辉 <Andy@tangchuhuidembp.lan>
2016-03-14 16:52:39 +0800

    CREATE

---
+++ SimplestopWATCH/SimplestopWATCH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimplestopWATCH
+//
+//  Created by 唐川辉 on 16/3/14.
+//  Copyright © 2016年 tch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimplestopWATCH/SimplestopWATCH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimplestopWATCH
+//
+//  Created by 唐川辉 on 16/3/14.
+//  Copyright © 2016年 tch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
